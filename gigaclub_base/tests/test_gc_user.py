22d32b33b93425c7dbc385a26bcd8ad2f8475ffb
Kevin <xxkevtvxx@gmail.com>
2022-03-19 16:03:57 +0100

    test

--- gigaclub_base/tests/test_gc_user.py
+++ gigaclub_base/tests/test_gc_user.py
@@ -4,4 +4,4 @@ from odoo.tests import SavepointCase
 class TestGCUser(SavepointCase):
     def test_method(self):
         res = self.env["gc.user"].test()
-        self.assertEqual(res, "test123", "res should be test.")
+        self.assertEqual(res, "test1234", "res should be test.")
