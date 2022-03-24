561d2c4d076fe8fcc506d88721ff55f51a726545
Kevin <xxkevtvxx@gmail.com>
2022-03-24 07:00:46 +0100

    more tests

--- gigaclub_team/tests/test_gc_team.py
+++ gigaclub_team/tests/test_gc_team.py
@@ -50,3 +50,41 @@ class TestGCTeam(SavepointCase):
         self.assertEqual(res, 2, "res should be 2")
         res = GCTeam.create_team("test2", "Test2")
         self.assertEqual(res, 3, "res should be 3")
+
+    def test_edit_team(self):
+        GCTeam = self.env["gc.team"]
+        GCUser = self.env["gc.user"]
+        self.team.manager_ids |= self.user
+        res = GCTeam.edit_team("test", "test", "test2", "description")
+        self.assertEqual(res, 0, "res should be 0")
+        self.assertEqual(self.team.name, "test2", "Team name should be test2")
+        self.assertEqual(
+            self.team.description,
+            "description",
+            "Team description should be description",
+        )
+        user2 = GCUser.create(
+            {
+                "name": "Test2",
+                "mc_uuid": "test2",
+            }
+        )
+        self.team.user_ids |= user2
+        res = GCTeam.edit_team("test2", "test2", "test3", "description")
+        self.assertEqual(res, 1, "res should be 1")
+
+    def test_leave_team(self):
+        GCTeam = self.env["gc.team"]
+        GCUser = self.env["gc.user"]
+        GCUser.create(
+            {
+                "name": "Test2",
+                "mc_uuid": "test2",
+            }
+        )
+        self.team.manager_ids |= self.user
+        res = GCTeam.leave_team("test")
+        self.assertEqual(res, 0, "res should be 0")
+        self.assertFalse(self.user.team_manager_id, "User should not be a manager")
+        res = GCTeam.leave_team("test2")
+        self.assertEqual(res, 1, "res should be 1")
