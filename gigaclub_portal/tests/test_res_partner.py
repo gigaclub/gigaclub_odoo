99df3c0fd77c5d8877f6d34c39c05f3670c6ae16
Kevin <xxkevtvxx@gmail.com>
2022-03-29 21:19:01 +0200

    added tests

---
+++ gigaclub_portal/tests/test_res_partner.py
@@ -0,0 +1,27 @@
+from odoo.tests import SavepointCase
+
+
+class TestResPartner(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env.ref("base.partner_demo")
+        cls.gc_user = cls.env["gc.user"].create(
+            {
+                "name": "test",
+            }
+        )
+
+    def test__inverse_name(self):
+        self.partner.gc_user_id = self.gc_user
+        self.assertEqual(self.partner.gc_user_id.name, "test", "Name should be test")
+        self.partner.name = "partnername"
+        self.assertEqual(
+            self.partner.gc_user_id.name, "partnername", "Name should be partnername"
+        )
+
+    def test_name_get(self):
+        name = self.partner.name_get()
+        self.assertEqual(
+            name[0][1], self.partner.name, "Name should be the name of the Partner"
+        )
