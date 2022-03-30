7e205d6a5e0156e4c2783a17fbe60088e008e60e
Kevin <xxkevtvxx@gmail.com>
2022-03-30 18:56:12 +0200

    added tests

---
+++ gigaclub_project/tests/test_project_task.py
@@ -0,0 +1,15 @@
+from odoo.tests import SavepointCase
+
+
+class TestProjectTask(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project_task = cls.env.ref("project.project_task_1")
+
+    def test_get_all_tasks(self):
+        self.assertTrue(self.project_task.get_all_tasks())
+
+    def test_get_task(self):
+        self.assertTrue(self.project_task.get_task(self.project_task.id))
+        self.assertFalse(self.project_task.get_task(False))
