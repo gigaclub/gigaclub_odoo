81417f9b8d12527e5b1bd64d53fbd85e33fb4fc3
Kevin <xxkevtvxx@gmail.com>
2022-04-01 07:46:22 +0200

    fix unset world_type

--- gigaclub_builder_system/models/gc_builder_world.py
+++ gigaclub_builder_system/models/gc_builder_world.py
@@ -46,9 +46,14 @@ class GCBuilderWorld(models.Model):
     def create_as_user(self, player_uuid, task_id, name, world_type):
         user_id = self.env["gc.user"].search([("mc_uuid", "=", player_uuid)])
         task_id = self.env["project.task"].browse(task_id)
-        world_type_id = self.env["gc.builder.world.type"].search(
-            [("name", "=ilike", world_type)]
-        )
+        if world_type:
+            world_type_id = self.env["gc.builder.world.type"].search(
+                [("name", "=ilike", world_type)], limit=1
+            )
+        else:
+            world_type_id = self.env["gc.builder.world.type"].search(
+                [("default", "=", True)], limit=1
+            )
         return self.create(
             {
                 "name": name,
@@ -67,9 +72,14 @@ class GCBuilderWorld(models.Model):
         elif user_id.team_user_id:
             team_id = user_id.team_user_id
         task_id = self.env["project.task"].browse(task_id)
-        world_type_id = self.env["gc.builder.world.type"].search(
-            [("name", "=ilike", world_type)]
-        )
+        if world_type:
+            world_type_id = self.env["gc.builder.world.type"].search(
+                [("name", "=ilike", world_type)], limit=1
+            )
+        else:
+            world_type_id = self.env["gc.builder.world.type"].search(
+                [("default", "=", True)], limit=1
+            )
         return self.create(
             {
                 "name": name,
