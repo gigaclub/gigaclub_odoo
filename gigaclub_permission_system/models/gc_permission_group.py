8670f61f83d76c7f4b22b42d0ee2d760490da9ed
Kevin <xxkevtvxx@gmail.com>
2022-06-03 21:41:09 +0200

    wip

--- gigaclub_permission_system/models/gc_permission_group.py
+++ gigaclub_permission_system/models/gc_permission_group.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class GCPermissionGroup(models.Model):
@@ -8,3 +8,11 @@ class GCPermissionGroup(models.Model):
     name = fields.Char()
     description = fields.Text()
     permission_profile_ids = fields.Many2many(comodel_name="gc.permission.profile")
+    global_group = fields.Boolean()
+
+    @api.model
+    def get_all_groups(self):
+        return [
+            {"id": x.id, "name": x.name, "description": x.description}
+            for x in self.search([("global_group", "=", True)])
+        ]
