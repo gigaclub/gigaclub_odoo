1974b896ed33e3178f93754f313b07da9842bc35
Kevin <kevin@fritsch.vip>
2022-06-13 06:30:30 +0200

    missing api.model

--- gigaclub_builder_system/models/gc_permission_connector.py
+++ gigaclub_builder_system/models/gc_permission_connector.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class GCPermissionConnector(models.Model):
@@ -6,12 +6,14 @@ class GCPermissionConnector(models.Model):
 
     world_id = fields.Many2one(comodel_name="gc.builder.world", index=True)
 
+    @api.model
     def get_team_worlds(self, team_id):
         return [
             self.env["gc.builder.world"].return_world(x)
             for x in self.search([("team_id", "=", team_id)]).mapped("world_id")
         ]
 
+    @api.model
     def get_user_worlds(self, player_uuid):
         user = self.env["gc.user"].search([("mc_uuid", "=", player_uuid)])
         if user:
