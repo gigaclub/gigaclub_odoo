81892fc3ae0d768ce3940cb520771dd4d4973242
Kevin <kevin@fritsch.vip>
2022-06-25 10:35:48 +0200

    added widgets field

--- gigaclub_translation/models/gc_translation_entry.py
+++ gigaclub_translation/models/gc_translation_entry.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class GCTranslationEntry(models.Model):
@@ -10,8 +11,22 @@ class GCTranslationEntry(models.Model):
         return self.env["res.lang"].get_installed()
 
     content = fields.Serialized()
+    widgets = fields.Serialized(compute="_compute_widgets", store=True)
     translation_ids = fields.Many2many(comodel_name="gc.translation")
     lang = fields.Selection(selection=_get_languages, required=True)
 
     # inverse for entry_ids in gc.language
     translation_id = fields.Many2one(comodel_name="gc.translation")
+
+    @api.depends("translation_ids.values")
+    def _compute_widgets(self):
+        for rec in self:
+            values = rec.translation_ids.mapped("values")
+            widgets = {"params": [], "list": []}
+            for value in values:
+                value = safe_eval(value)
+                if "params" in value and value["params"]:
+                    widgets["params"].extend(list(value["params"].keys()))
+                if "list" in value and value["list"]:
+                    widgets["list"].extend(list(value["list"].keys()))
+            rec.widgets = widgets
