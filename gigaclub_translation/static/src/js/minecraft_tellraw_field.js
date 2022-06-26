6004ea102e08d8266f23cffe5511e81a32b807fd
Kevin <kevin@fritsch.vip>
2022-06-26 08:35:31 +0200

    added new dialog for list widgets

--- gigaclub_translation/static/src/js/minecraft_tellraw_field.js
+++ gigaclub_translation/static/src/js/minecraft_tellraw_field.js
@@ -2,9 +2,27 @@ odoo.define("gigaclub_translation.minecraft_tellraw_field", function (require) {
   "use strict";
 
   const minecraftTellrawFieldModule = require("minecraft_tellraw_field.minecraft_tellraw_field");
+  const OwlDialog = require("web.OwlDialog");
   const minecraftTellrawField = minecraftTellrawFieldModule.MinecraftTellrawField;
   const minecraftTellrawDialog = minecraftTellrawFieldModule.MinecraftTellrawDialog;
+  const {Component} = owl;
   const {useState} = owl.hooks;
+
+  class MinecraftTellrawListWidgetDialog extends Component {
+    constructor(...args) {
+      super(...args);
+      this.state = useState({
+        values: ["", {text: "placeholder", listitem: true}],
+      });
+    }
+  }
+
+  Object.assign(MinecraftTellrawListWidgetDialog, {
+    components: {
+      Dialog: OwlDialog,
+    },
+    template: "MinecraftTellrawListWidgetDialog",
+  });
 
   class MinecraftTellrawDialog extends minecraftTellrawDialog {
     constructor(...args) {
@@ -22,18 +40,29 @@ odoo.define("gigaclub_translation.minecraft_tellraw_field", function (require) {
     constructor(...args) {
       super(...args);
       this.state = useState(
-        Object.assign({}, this.state, {widgets: {params: [], list: []}})
+        Object.assign({}, this.state, {
+          widgets: {params: [], list: []},
+          minecraftTellrawListWidgetDialog: false,
+        })
       );
     }
     willStart() {
       this.state.widgets = this.props.record.data.widgets;
+    }
+    openList() {
+      this.state.minecraftTellrawListWidgetDialog = true;
+    }
+    onListWidgetDialogClosed() {
+      this.state.minecraftTellrawListWidgetDialog = false;
     }
   }
 
   Object.assign(MinecraftTellrawField, {
     components: {
       MinecraftTellrawDialog,
+      MinecraftTellrawListWidgetDialog,
     },
+    template: "FieldMinecraftTellrawInheritGigaClubTranslation",
   });
 
   return {
