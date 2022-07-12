b657ad595403863ef4296e4506a6318a44fafc4a
Kevin <kevin@fritsch.vip>
2022-07-12 06:21:47 +0200

    initial commit

---
+++ gigaclub_developer_system/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GigaClub Developer System",
+    "version": "14.0.1.0.0",
+    "category": "GigaClub",
+    "author": "GigaClub.net",
+    "website": "https://GigaClub.net/",
+    "license": "AGPL-3",
+    "depends": ["gigaclub_github", "gigaclub_project"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ gigaclub_github/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GigaClub GitHub",
+    "version": "14.0.1.0.0",
+    "category": "GigaClub",
+    "author": "GigaClub.net",
+    "website": "https://GigaClub.net/",
+    "license": "AGPL-3",
+    "depends": ["github_connector", "gigaclub_team"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
