23e20b4423e7b1df146fbaf6075af14f44b8a796
Kevin <kevin@fritsch.vip>
2022-08-28 14:59:20 +0200

    initial commit

---
+++ gigaclub_portal_team/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GigaClub Portal Team",
+    "version": "14.0.1.0.0",
+    "category": "GigaClub",
+    "author": "GigaClub.net",
+    "website": "https://GigaClub.net/",
+    "license": "AGPL-3",
+    "depends": ["gigaclub_portal"],
+    "data": ["views/portal_templates.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
