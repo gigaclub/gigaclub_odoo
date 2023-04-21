1023845ebb2c9c56c416e58e8c890a008631962e
Kevin <kevin@fritsch.vip>
2023-04-21 10:31:18 +0200

    WIP

--- gigaclub_portal_team/static/src/js/gigaclub_portal_team.esm.js
+++ gigaclub_portal_team/static/src/js/gigaclub_portal_team.esm.js
@@ -35,3 +35,26 @@ $("#deletePermissionGroupConfirmModal").on("show.bs.modal", (event) => {
   const permissionGroupId = button.getAttribute("data-bs-permission_group_id");
   window.currentPermissionGroupId = permissionGroupId;
 });
+
+$("#user-modal").on("show.bs.modal", (event) => {
+  const button = event.relatedTarget;
+  let data = button.getAttribute("data-bs-dict");
+  if (data) {
+    data = JSON.parse(data);
+    console.log(data);
+    // Fill in the user select box with the selected user's name and ID
+    $("#select-user option[value='" + data.id + "']").prop("selected", true);
+    //    $("#select-user").selectpicker("refresh");
+
+    // Fill in the groups select box with the selected user's groups
+    const group_ids = data.groups.map((group) => group.id);
+    $("#select-groups").val(group_ids);
+    //    $("#select-groups").trigger("change");
+
+    // Fill in the permission checkboxes with the selected user's permissions
+    $("#input-user-editteam").prop("checked", data.edit_team);
+    $("#input-user-inviteuser").prop("checked", data.invite_user);
+    $("#input-user-kickuser").prop("checked", data.kick_user);
+    $("#input-user-createworldasteam").prop("checked", data.create_world_as_team);
+  }
+});
