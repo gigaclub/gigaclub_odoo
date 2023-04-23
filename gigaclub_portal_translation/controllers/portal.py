6390070d7e37cf6dae772044de4a34af9d10de8f
Kevin <kevin@fritsch.vip>
2023-04-23 19:57:50 +0200

    added button to portal start page...

--- gigaclub_portal_translation/controllers/portal.py
+++ gigaclub_portal_translation/controllers/portal.py
@@ -1,5 +1,14 @@
+from odoo.http import request
+
 from odoo.addons.gigaclub_portal.controllers.portal import GigaClubPortal
 
 
 class GigaClubTranslationPortal(GigaClubPortal):
-    ...
+    def _prepare_home_portal_values(self, counters):
+        values = super()._prepare_home_portal_values(counters)
+        if "translation_count" in counters:
+            user = request.env.user.partner_id.gc_user_id
+            values["translation_count"] = (
+                user and request.env["gc.translation"].search_count([]) or 0
+            )
+        return values
