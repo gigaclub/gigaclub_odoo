9f35cb8aade9dc6c9ae409849b436612c39cd682
Kevin <kevin@fritsch.vip>
2023-04-29 12:21:29 +0200

    Add second option for minecraft authentication and an option for discord authentication to portal (#69)
    * preparations
    
    * use queue job
    
    * added account authentication over minecraft server
    
    * added discord authentication with portal

--- gigaclub_base/models/gc_user.py
+++ gigaclub_base/models/gc_user.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+import random
+import string
+
+from odoo import api, fields, models
 
 
 class GCUser(models.Model):
@@ -10,6 +13,7 @@ class GCUser(models.Model):
     state = fields.Selection(
         selection=[("online", "Online"), ("offline", "Offline")], default="offline"
     )
+    auth_token = fields.Char()
 
     _sql_constraints = [
         ("mc_uuid_unique", "UNIQUE(mc_uuid)", "MC_UUID must be unique!")
@@ -20,3 +24,22 @@ class GCUser(models.Model):
         for rec in self:
             res.append((rec.id, f"({rec.name}) - {rec.mc_uuid}"))
         return res
+
+    @api.model
+    def generate_auth_token(self, mc_uuid):
+        user = self.search([("mc_uuid", "=", mc_uuid)], limit=1)
+        if user.auth_token:
+            return user.auth_token
+
+        # Define the characters to use in the token
+        characters = string.ascii_letters + string.digits
+
+        # Generate a 6-length token
+        user.auth_token = "".join(random.choice(characters) for i in range(6))
+
+        user.with_delay(eta=5 * 60).remove_auth_token()
+
+        return user.auth_token
+
+    def remove_auth_token(self):
+        self.update({"auth_token": False})
