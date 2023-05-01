ef7fecaf7606e77deeda61ca15cdf28a60c2db76
Kevin <kevin@fritsch.vip>
2023-05-01 15:53:55 +0200

    added requests portal

---
+++ gigaclub_portal_requests/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GigaClub Portal Requests",
+    "version": "16.0.1.0.0",
+    "category": "GigaClub",
+    "author": "GigaClub",
+    "website": "https://github.com/GigaClub/gigaclub_odoo",
+    "license": "AGPL-3",
+    "depends": ["gigaclub_portal_builder_system", "gigaclub_portal"],
+    "data": ["views/portal_templates.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
