43a9fb563c4ccaa37cf72b969b47b38500785eaf
Kevin <kevin@fritsch.vip>
2023-05-07 17:10:44 +0200

    initial commit

---
+++ gigaclub_exception_reporting/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GigaClub Exception Reporting",
+    "version": "16.0.1.0.0",
+    "category": "GigaClub",
+    "author": "GigaClub",
+    "website": "https://github.com/GigaClub/gigaclub_odoo",
+    "license": "AGPL-3",
+    "depends": ["gigaclub_base"],
+    "data": ["views/gc_exception_views.xml", "views/menu_views.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
