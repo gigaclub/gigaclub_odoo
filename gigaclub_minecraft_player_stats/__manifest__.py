c26000bd225b8e08b2b80c4949f4aa9ad437e0ef
Kevin <kevin@fritsch.vip>
2023-05-16 20:20:03 +0200

    added views

--- gigaclub_minecraft_player_stats/__manifest__.py
+++ gigaclub_minecraft_player_stats/__manifest__.py
@@ -6,7 +6,12 @@
     "website": "https://github.com/GigaClub/gigaclub_odoo",
     "license": "AGPL-3",
     "depends": ["gigaclub_base"],
-    "data": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/gc_minecraft_player_stats_views.xml",
+        "views/gc_minecraft_server_views.xml",
+        "views/gc_minecraft_stats_views.xml",
+    ],
     "demo": [],
     "installable": True,
     "auto_install": False,
